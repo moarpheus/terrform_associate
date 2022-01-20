08a16842f9a465a848e124135f897046e3ee6d02
JG <moarpheus@gmail.com>
2022-01-20 13:26:01 +0000

    using vars in the main

--- main.tf
+++ main.tf
@@ -1,20 +1,26 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "3.72.0"
     }
   }
 }
+
+variable "instance_type" {
+  type = string
+}
+
 
 provider "aws" {
   profile = "default"
   region  = "eu-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = ""
-  instance_type = "t2.micro"
+resource "aws_instance" "my_server" {
+  ami = "ami-0fdbd8587b1cf431e"
+
+  instance_type = "t2.nano"
 
   tags = {
     Name = "MyServer"
