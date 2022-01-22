1fe36d46163593632eb79e73971720ff27648656
JG <moarpheus@gmail.com>
2022-01-22 10:35:55 +0000

    added vars and providers files

--- main.tf
+++ main.tf
@@ -1,31 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.72.0"
-    }
-  }
-}
-
-variable "instance_type" {
-  type = string
-}
-
-
-provider "aws" {
-  profile = "default"
-  region  = "eu-west-2"
-}
-
-locals {
-  instance_type = "t3.micro"
-}
-
-
 resource "aws_instance" "my_server" {
   ami = "ami-0fdbd8587b1cf431e"
 
-  instance_type = local.instance_type
+  instance_type = var.instance_type
 
   tags = {
     Name = "MyServer"
