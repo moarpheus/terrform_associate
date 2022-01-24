34317fe7a03dd8fb73bc05c1893e5f4e7f10d2d5
JG <moarpheus@gmail.com>
2022-01-24 15:04:14 +0000

    added instance

--- main.tf
+++ main.tf
@@ -9,4 +9,14 @@ terraform {
 
 provider "aws" {
   # Configuration options
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0fdbd8587b1cf431e"
+  instance_type = "t2.nano"
+
+  tags = {
+    Name = "MyServer"
+  }
 }
