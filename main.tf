01d9e1b21f95381c7db8159aca1f311bd8f75097
JG <moarpheus@gmail.com>
2022-01-24 07:51:29 +0000

    added security group to vpc and server

--- main.tf
+++ main.tf
@@ -3,7 +3,42 @@ resource "aws_instance" "my_server" {
 
   instance_type = var.instance_type
 
+  security_groups = [aws_security_group.allow_ssh.id]
+
   tags = {
     Name = "MyServer"
+  }
+}
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic from a single 212.3.197.246"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description      = "SSH from my IP"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["212.3.197.246/32"]
+    ipv6_cidr_blocks = []
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
   }
 }
