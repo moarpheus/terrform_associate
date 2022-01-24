7708b45af41fbea7c15e923e1db5caf9bff2ad04
JG <moarpheus@gmail.com>
2022-01-24 15:13:54 +0000

    added security group

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ terraform {
 }
 
 provider "aws" {
-  # Configuration options
   region = "eu-west-2"
 }
 
@@ -16,7 +15,35 @@ resource "aws_instance" "web" {
   ami           = "ami-0fdbd8587b1cf431e"
   instance_type = "t2.nano"
 
+  key_name = "home_2"
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
   tags = {
     Name = "MyServer"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "SSH to VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
   }
 }
