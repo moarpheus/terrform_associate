ced6cf230ecce64b5304a86bca7b6011c33c9398
JG <moarpheus@gmail.com>
2022-02-03 09:09:09 +0000

    starting fresh

--- main.tf
+++ main.tf
@@ -11,39 +11,22 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0fdbd8587b1cf431e"
-  instance_type = "t2.nano"
+data "aws_ami" "example" {
+  most_recent      = true
+  owners           = ["amazon"]
 
-  key_name = "home_2"
-
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-
-  tags = {
-    Name = "MyServer"
-  }
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    description      = "SSH to VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
   }
 
-  tags = {
-    Name = "allow_ssh"
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
